8de3bce38e55732c8d75606774386845f00921d8
lx071 <onlyfor_ever@163.com>
2022-09-17 07:50:45 -0700

    update

--- example_python/pybind11-project/harness_example/harness_example_v3.0/hdl/bfm.v
+++ example_python/pybind11-project/harness_example/harness_example_v3.0/hdl/bfm.v
@@ -24,29 +24,58 @@ tinyalu inst_alu(
 	.done(done),
 	.result(res_o)
 );
-int num=0;
+
+
 import "DPI-C" function void recv (input int data);
 import "DPI-C" function void c_py_gen_packet(output bit[6143:0] pkt);
 
+
 bit[6143:0] data;
+reg xmit_en=0;
+
+reg[15:0]   num = 0;
+int clk_num=0;
+int flag=0;
+int message_num = 0;
 
 always @(posedge clk_i) begin
-    if(num<=10) num = num + 1;
-    if(num==10) begin
-        reset_i=1;
-        recv(456);
-        c_py_gen_packet(data);
-        $display("get data[0] ='h%h",data[7:0]);
-        $display("get data[1] ='h%h",data[15:8]);
-        $display("get data[2] ='h%h",data[23:16]);
+    if(clk_num<=10) clk_num = clk_num + 1;
+    if(clk_num==10) reset_i=1;
 
-        $display("get data[3] ='h%h",data[31:24]);
-        $display("get data[4] ='h%h",data[39:32]);
-        $display("get data[5] ='h%h",data[47:40]);
+    if(reset_i) begin
+        //num = 0;
+        //$display("num = %h",num);
 
-        $display("get data[6] ='h%h",data[55:48]);
-        $display("get data[7] ='h%h",data[63:56]);
-        $display("get data[8] ='h%h",data[71:64]);
+        if(flag == 0) begin
+            recv(456);
+            c_py_gen_packet(data);
+            xmit_en = 1;
+            flag = flag + 1;
+            message_num = message_num + 1;
+        end
+
+        if(xmit_en) begin
+            A_s = data[7:0];
+            B_s = data[15:8];
+            op = data[18:16];
+
+            $display("get data[0] = %d",A_s);
+            $display("get data[1] = %d",B_s);
+            $display("get data[2] = %d",op);
+            //$display("get data[0] ='h%h",A_s);
+            //$display("get data[1] ='h%h",B_s);
+            //$display("get data[2] ='h%h",op);
+
+            data = (data >> 24);
+            num = num + 1;
+            //$display("num = %h",num);
+            //$display("xmit_en = %h",xmit_en);
+        end
+
+        if(num>=256) begin
+            num = 0;
+            xmit_en = xmit_en - 1;
+        end
     end
 
 end
