ad0f1b795f458add5e03fc1ae3b2b18640936ea5
lx071 <onlyfor_ever@163.com>
2022-09-19 02:18:18 -0700

    update

---
+++ example_python/pybind11-project/harness_example/harness_example_v3.2/test.py
@@ -0,0 +1,46 @@
+from utils.harness_utils import sim
+import time
+import threading
+
+
+def do_clk_test():
+    time1 = time.time()
+    s = sim('./hdl/', 'bfm.v')
+    time2 = time.time()
+    s.disableWave()
+    # 设置获取message数据的回调函数名
+    s.set_send_message_func("send_msg")
+
+    def a():
+        # 设置时钟信息(时钟端口名、时钟周期、运行周期数)  10ps*300=3000ps=3ns
+        s.set_clk_info("clk_i", 10, 10010)
+
+        s.deleteHandle()
+        time3 = time.time()
+        print('compile time:', time2 - time1)
+        print('simulation time:', time3 - time2)
+        pass
+
+    def b():
+
+        print("func b()")
+        pass
+
+    def func():
+        t1 = threading.Thread(target=a, args=())
+        t2 = threading.Thread(target=b, args=())
+        # b()
+        # a()
+
+        t1.start()
+        t2.start()
+
+        pass
+
+    func()
+
+
+if __name__ == '__main__':
+    do_clk_test()
+    pass
+
