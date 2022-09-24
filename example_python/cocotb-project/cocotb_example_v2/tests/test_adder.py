73eae5797e7785ed95e5be5830a05dc4bf4eea63
lx071 <onlyfor_ever@163.com>
2022-09-24 07:56:44 -0700

    update

---
+++ example_python/cocotb-project/cocotb_example_v2/tests/test_adder.py
@@ -0,0 +1,25 @@
+# This file is public domain, it can be freely copied without restrictions.
+# SPDX-License-Identifier: CC0-1.0
+# Simple tests for an adder module
+
+import cocotb
+from cocotb.triggers import FallingEdge, RisingEdge
+import random
+
+@cocotb.test()
+async def adder_basic_test(dut):
+    """Test for 5 + 10"""
+
+    await FallingEdge(dut.reset_i)
+
+    # 2000packages = 2000 * 100 data
+    for k in range(2000):
+        data_package = 0
+        # 2bytes * 100 = 200bytes = 1600bit 
+        for j in range(100):
+            data_item = j % 200
+            data_package = (data_package << 16) + (data_item << 8) + data_item
+            pass
+        dut.data = data_package
+        dut.xmit_en.value = 1
+        await FallingEdge(dut.xmit_en)
