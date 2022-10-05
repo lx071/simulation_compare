94310edf5f8e9eadc328979f0b38ee028d90b9d3
lx071 <onlyfor_ever@163.com>
2022-10-05 04:36:29 -0700

    update

---
+++ example_python/cocotb-project/cocotb_example_v1.0/hdl/bfm.v
@@ -0,0 +1,71 @@
+`timescale 1ns/1ps
+
+module bfm(
+//input   clk_i,
+//input   reset_i,
+output  reg [7:0] res_o
+);
+
+reg [7:0] A_s;
+reg [7:0] B_s;
+
+parameter TOTAL_WIDTH=256;
+
+bit clk_i, reset_i;
+
+always #5 clk_i = ~clk_i;
+
+reg xmit_en = 0;
+reg [3199:0] data;
+int num = 0;
+int clk_num = 0;
+
+initial begin
+    clk_i = 0;
+    reset_i = 1;
+    A_s = 0;
+    B_s = 0;
+    data = 0;
+end
+
+MyTopLevel inst_add(
+    .io_A(A_s),
+    .io_B(B_s),
+    .io_X(res_o),
+    .clk(clk_i),
+    .reset(reset_i)
+);
+
+always @(posedge clk_i) begin
+
+    if(clk_num<=10) begin
+        clk_num = clk_num + 1;
+    end 
+    if(clk_num==10) begin
+        reset_i = 0;
+    end
+
+    if(reset_i) begin
+        A_s <= 8'h0;
+        B_s <= 8'h0;
+    end else begin   
+        if(xmit_en) begin
+            A_s <= data[7:0];
+            B_s <= data[15:8];
+            data = (data >> 16);
+            num = num + 1;
+        end    
+        if(num >= 100) begin
+            num = 0;
+            xmit_en = xmit_en - 1;
+        end 
+    end
+end
+
+
+initial begin
+    //$dumpfile("dump.vcd");
+    //$dumpvars;
+end
+
+endmodule
---
+++ example_python/cocotb-project/cocotb_example_v1.0/tests/test_adder.py
@@ -0,0 +1,26 @@
+# This file is public domain, it can be freely copied without restrictions.
+# SPDX-License-Identifier: CC0-1.0
+# Simple tests for an adder module
+
+import cocotb
+from cocotb.triggers import FallingEdge, RisingEdge
+import random
+
+@cocotb.test()
+async def adder_basic_test(dut):
+    """Test for 5 + 10"""
+    # 200000times = 2000 * 100(1600bit) = 10000 * 20(320bit) = 1000 * 200(3200bit)
+
+    await FallingEdge(dut.reset_i)
+
+    # 2000packages = 2000 * 100 data
+    for k in range(2000):
+        data_package = 0
+        # 2bytes * 100 = 200bytes = 1600bit 
+        for j in range(100):
+            data_item = j % 200
+            data_package = (data_package << 16) + (data_item << 8) + data_item
+            pass
+        dut.data.value = data_package
+        dut.xmit_en.value = 1
+        await FallingEdge(dut.xmit_en)
