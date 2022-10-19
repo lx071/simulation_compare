2264ab9561a1d35bc507af8bb73ac30b141b8dfe
lx071 <onlyfor_ever@163.com>
2022-10-19 00:55:39 -0700

    update

--- example_python/cocotb-project/cocotb_example_v0/cocotb_example_v0.2/tests/test_adder.py
+++ example_python/cocotb-project/cocotb_example_v0/cocotb_example_v0.2/tests/test_adder.py
@@ -14,10 +14,10 @@ async def adder_basic_test(dut):
     await FallingEdge(dut.reset_i)
 
     # 2000packages = 2000 * 100 data
-    for k in range(40000):
+    for k in range(10000):
         data_package = 0
         # 2bytes * 100 = 200bytes = 1600bit 
-        for j in range(50):
+        for j in range(200):
             data_item = j % 200
             data_package = (data_package << 16) + (data_item << 8) + data_item
             pass
