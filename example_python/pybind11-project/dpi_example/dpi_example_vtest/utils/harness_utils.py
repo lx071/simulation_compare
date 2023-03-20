d4ca066a6d1aa8f6135b9f3f414fb93517ec22c0
lx071 <onlyfor_ever@163.com>
2023-03-20 07:09:38 -0700

    update

---
+++ example_python/pybind11-project/dpi_example/dpi_example_vtest/utils/harness_utils.py
@@ -0,0 +1,36 @@
+import os
+# import random
+import subprocess
+import re
+
+
+def do_python_api():
+    print('do_python_api')
+    return 0
+
+
+def add(a, b):
+    return a + b
+
+
+def recv(data):
+    print('recv_python:', data)
+
+
+def send_msg():
+    data_all = 0
+    # 01 02 03 ... 20
+    for i in range(512):
+        # data = random.randint(1, 100)
+        # print(data)
+        data = i % 100
+        data_all = (data_all << 8) + data
+
+    bytes_val = data_all.to_bytes(512, 'big')
+    return bytes_val
+    pass
+
+
+
+if __name__ == '__main__':
+    pass
