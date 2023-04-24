3efca623e206f78f429cce265bfda0e715d8a62d
lx071 <onlyfor_ever@163.com>
2023-04-24 07:47:32 -0700

    update

---
+++ example_c/sc_example_vtest/tlm_example_v0/sim_main.cpp
@@ -0,0 +1,178 @@
+// -*- SystemC -*-
+// DESCRIPTION: Verilator Example: Top level main for invoking SystemC model
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2017 by Wilson Snyder.
+// SPDX-License-Identifier: CC0-1.0
+//======================================================================
+
+// SystemC global header
+#include <systemc.h>
+
+// Include common routines
+#include <verilated.h>
+
+// Include model header, generated from Verilating "top.v"
+#include "Vwrapper.h"
+
+#include <tlm.h>
+#include <tlm_utils/simple_initiator_socket.h>
+#include <tlm_utils/simple_target_socket.h>
+
+#include "svdpi.h"
+#include "Vwrapper__Dpi.h"
+#include <iostream>
+
+using namespace std;
+
+extern void set_data(const svBitVecVal* data);
+//extern svBit get_xmit_en();
+
+class Target : sc_module{
+public:
+    tlm_utils::simple_target_socket<Target> socket;
+
+    Target(sc_module_name name, Vwrapper* top) : sc_module(name), top_(top) {
+        socket.register_b_transport(this, &Target::b_transport);   //register methods with each socket
+    }
+
+private:
+
+    Vwrapper* top_;
+    int count;
+    // char *payload_data;
+    unsigned char* payload_data = nullptr;
+
+    void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay) {
+        tlm::tlm_command cmd = trans.get_command();
+        sc_dt::uint64 addr = trans.get_address();
+        unsigned char* data = trans.get_data_ptr();
+        unsigned int len = trans.get_data_length();
+        unsigned char* byte_en = trans.get_byte_enable_ptr();
+        unsigned int wid = trans.get_streaming_width();
+
+        if (addr != 0x0) {
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return;
+        }
+
+        if (cmd == tlm::TLM_READ_COMMAND) {
+            //std::cout << "read_len:" << len << std::endl;
+            if (len != sizeof(count)) {
+                trans.set_response_status(tlm::TLM_BURST_ERROR_RESPONSE);
+                return;
+            }
+            memcpy(data, &count, sizeof(count));
+            trans.set_response_status(tlm::TLM_OK_RESPONSE);
+        } else if (cmd == tlm::TLM_WRITE_COMMAND) {
+            payload_data = data;
+            //std::cout << "write_len:" << len << std::endl;
+            // for(int i=0;i<len;i++) cout << std::hex << static_cast<int>(*(payload_data + i)) << endl;
+
+            //  ‘const svBitVecVal*’ {aka ‘const unsigned int*’}
+            const unsigned int* sv_data = reinterpret_cast<const unsigned int*>(payload_data);
+            set_data(sv_data);
+            
+            trans.set_response_status(tlm::TLM_OK_RESPONSE);
+        } else {
+            trans.set_response_status(tlm::TLM_COMMAND_ERROR_RESPONSE);
+            return;
+        }
+    }
+};
+
+class Initiator : sc_module{
+public:
+    tlm_utils::simple_initiator_socket<Initiator> socket;
+
+    Initiator(sc_module_name name) : sc_module(name) {
+        
+    }
+
+
+};
+
+
+// typedef unsigned __int32 uint32_t;
+// typedef uint32_t svBitVecVal;
+void send_tlm_data(Initiator *initiator, int num) 
+{
+    tlm::tlm_generic_payload trans;
+    // sc_time delay = sc_time(10, SC_NS);
+
+    sc_time delay = SC_ZERO_TIME;
+    //int num = 50;
+    unsigned char arr[num*2];
+
+    for (int i = 0; i < num; i = i + 1) {
+        arr[i*2] = i%100;
+        arr[i*2+1] = i%100;
+    }
+    // unsigned char arr[] = {0x1, 0x2, 0x3, 0x4, 0x5};
+    unsigned char *payload_data = arr;
+    // char *payload_data = "\x11\x22\x33\x44\x44";
+
+    // cout << "sizeof(*payload_data)" << sizeof(*payload_data) << endl;
+    // cout << "sizeof(payload_data)" << sizeof(payload_data) << endl; //指针占8字节
+    // cout << "strlen(payload_data)" << strlen((const char*)payload_data) << endl;
+
+    // set data
+    trans.set_command(tlm::TLM_WRITE_COMMAND);
+    trans.set_address(0x0);
+    trans.set_data_ptr(reinterpret_cast<unsigned char*>(payload_data));
+    trans.set_data_length(strlen((const char*)payload_data));
+    initiator->socket->b_transport(trans, delay);
+
+    assert(trans.is_response_ok());
+
+    // memcpy(data, payload_data, 5);
+}
+
+
+void recv(int data) 
+{
+    std::cout<<"recv:"<<data<<std::endl;
+}
+
+int sc_main(int argc, char* argv[]) {
+    
+    std::unique_ptr<VerilatedContext> contextp = std::make_unique<VerilatedContext>();
+    Vwrapper* top = new Vwrapper(contextp.get());
+
+    contextp->commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+    
+    Target target("target", top);
+    Initiator initiator("initiator");
+
+    initiator.socket.bind(target.socket);
+    
+    const svScope scope = svGetScopeFromName("TOP.wrapper");
+    assert(scope);  // Check for nullptr if scope not found
+    svSetScope(scope);
+
+    int NUM = 6;
+    int item_num = 100;
+    int num = 0;
+    int xmit_en = 1;
+
+    // Simulate until $finish
+    while (!Verilated::gotFinish()) {       
+ 
+        if(top->xmit_en == 1)
+        {
+            num = num + 1;
+            if(num >= NUM + 1) break;
+            send_tlm_data(&initiator, item_num);
+            
+        } 
+        top->eval();
+        contextp->timeInc(1000);
+    }
+
+    // Final model cleanup
+    top->final();
+    delete top;
+    // Return good completion status
+    return 0;
+}
