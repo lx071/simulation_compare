cba276487f5d24858e053465e4dc29230a7691d0
lx071 <onlyfor_ever@163.com>
2023-05-23 19:31:08 -0700

    update

--- example_c/tlm_example/tlm_example_g/tlm_example_v4/tb/arp_pybind/sim_main.cpp
+++ example_c/tlm_example/tlm_example_g/tlm_example_v4/tb/arp_pybind/sim_main.cpp
@@ -15,9 +15,6 @@
 #include <tlm_utils/simple_initiator_socket.h>
 #include <tlm_utils/simple_target_socket.h>
 
-// #include "verilated.h"
-// #include "Vbfm.h"
-
 using namespace std;
 //typedef unsigned char uint8_t;
 //typedef unsigned int uint32_t; 
@@ -31,9 +28,9 @@ py::scoped_interpreter guard;
 py::module_ utils;
 
 extern "C" void gen_tlm_data(int num);
-extern "C" void set_data(const svBitVecVal* data);
+extern "C" void set_data(const unsigned char* data);
 extern "C" void recv_tlm_data(int num);
-extern "C" void get_data(svBitVecVal* data);
+extern "C" void get_data(unsigned char* data);
 
 class Target : sc_module{
 public:
@@ -44,21 +41,42 @@ public:
         socket.register_transport_dbg(this, &Target::transport_dbg);
     }
 
+    unsigned char *output_payload_data = nullptr;
+
+    void recv_tlm_data(int num)
+    {
+        // cout << "recv_tlm_data" << endl;
+        tlm::tlm_generic_payload trans;
+
+        tlm::tlm_sync_enum status;
+        tlm::tlm_phase bw_phase;
+        sc_time delay;
+
+        // response_in_progress = true;
+        bw_phase = tlm::BEGIN_RESP;
+        delay = SC_ZERO_TIME;
+
+        output_payload_data = new unsigned char[42]; 
+
+        get_data(output_payload_data);
+        
+        trans.set_data_ptr(output_payload_data);
+
+        status = socket->nb_transport_bw( trans, bw_phase, delay );
+    } 
+
 private:
-    // char *payload_data;
-    unsigned char* input_payload_data = nullptr;
-    unsigned int *output_payload_data = nullptr;
 
     // TLM-2 blocking transport method
     virtual void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay) {
         tlm::tlm_command cmd = trans.get_command();
-        sc_dt::uint64 addr = trans.get_address();
-        unsigned char* data = trans.get_data_ptr();
-        unsigned int len = trans.get_data_length();
+        sc_dt::uint64    adr = trans.get_address() / 4;
+        unsigned char*   ptr = trans.get_data_ptr();
+        unsigned int     len = trans.get_data_length();
         unsigned char* byte_en = trans.get_byte_enable_ptr();
         unsigned int wid = trans.get_streaming_width();
 
-        if (addr != 0x0) {
+        if (adr != 0x0) {
             trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
             return;
         }
@@ -68,13 +86,8 @@ private:
             trans.set_response_status(tlm::TLM_OK_RESPONSE);
 
         } else if (cmd == tlm::TLM_WRITE_COMMAND) {
-            input_payload_data = data;
-            //std::cout << "write_len:" << len << std::endl;
-            // for(int i=0;i<len;i++) cout << std::hex << static_cast<int>(*(payload_data + i)) << endl;
 
-            //  ‘const svBitVecVal*’ {aka ‘const unsigned int*’}
-            const unsigned int* sv_data = reinterpret_cast<const unsigned int*>(input_payload_data);
-            set_data(sv_data);
+            set_data(ptr);
             
             trans.set_response_status(tlm::TLM_OK_RESPONSE);
         } else {
@@ -98,12 +111,11 @@ private:
         // Calculate the number of bytes to be actually copied
         // unsigned int num_bytes = (len < (SIZE - adr) * 4) ? len : (SIZE - adr) * 4;
     
-        output_payload_data = new unsigned int[11]; 
+        // output_payload_data = new unsigned char[42]; 
 
         if ( cmd == tlm::TLM_READ_COMMAND )
         {
-            get_data(output_payload_data);
-            memcpy(ptr, output_payload_data, 42);
+            get_data(ptr);
         }
             
         else if ( cmd == tlm::TLM_WRITE_COMMAND )
@@ -122,6 +134,8 @@ public:
     Initiator(sc_module_name name) : sc_module(name) {
         // SC_THREAD(run);     //Similar to a Verilog @initial block
 
+        socket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
+        
         py::module_ sys = py::module_::import("sys");
         py::list path = sys.attr("path");
         // path.attr("append")("../utils");    //for verilator
@@ -129,7 +143,9 @@ public:
         utils = py::module_::import("harness_utils");     
     }
 
-    unsigned char *payload_data;
+    unsigned char* input_payload_data = nullptr;
+    unsigned char *output_payload_data = nullptr;
+    unsigned char *ref_output = nullptr;
 
     void gen_tlm_data(int num)
     {
@@ -140,49 +156,50 @@ public:
 
         py::bytes result = utils.attr("send_data")();
         Py_ssize_t size = PyBytes_GET_SIZE(result.ptr());
-        unsigned char * payload_data = (unsigned char * )PyBytes_AsString(result.ptr());    //# low bit 01 02 03 ... 20 high bit
+        input_payload_data = (unsigned char * )PyBytes_AsString(result.ptr());    //# low bit 01 02 03 ... 20 high bit
 
         // set data
         trans.set_command(tlm::TLM_WRITE_COMMAND);
         trans.set_address(0x0);
-        trans.set_data_ptr(reinterpret_cast<unsigned char*>(payload_data));
-        trans.set_data_length(strlen((const char*)payload_data));
+        trans.set_data_ptr(reinterpret_cast<unsigned char*>(input_payload_data));
+        trans.set_data_length(strlen((const char*)input_payload_data));
         socket->b_transport(trans, delay);
 
         assert(trans.is_response_ok());
     }
 
+private:
 
-    void recv_tlm_data(int num)
+    // TLM-2 backward non-blocking transport method
+
+    virtual tlm::tlm_sync_enum nb_transport_bw( tlm::tlm_generic_payload& trans,
+                                                tlm::tlm_phase& phase, sc_time& delay )
     {
-        tlm::tlm_generic_payload trans;
-        // sc_time delay = sc_time(10, SC_NS);
 
-        sc_time delay = SC_ZERO_TIME;
+        // The timing annotation must be honored
+        // m_peq.notify( trans, phase, delay );
 
-        trans.set_command(tlm::TLM_READ_COMMAND);
-        trans.set_address(0);
-        trans.set_read();
-        trans.set_data_length(128);
+        // rx_payload_data ='h5a5152535455dad1d2d3d4d508060001080006040002dad1d2d3d4d5c0a801655a5152535455c0a80164
 
-        unsigned char* data = new unsigned char[42];
-        trans.set_data_ptr(data);
+        unsigned char*   ptr = trans.get_data_ptr();
+        // output_payload_data = new unsigned char[42];
+        output_payload_data = ptr;
 
-        unsigned int n_bytes = socket->transport_dbg( trans );
-    
-        size_t size_data = sizeof(data);
+        size_t size_data = sizeof(output_payload_data);
         
         auto res = py::array(py::buffer_info(
-            data,                              // 数据指针
+            output_payload_data,                              // 数据指针
             sizeof(char),                      // 元素大小
             py::format_descriptor<char>::value, // 格式化描述符
             1,                                  // 维度
-            { num },                           // 形状
+            { 42 },                           // 形状
             { sizeof(char) }                    // 每个维度的字节数
         ));
         
         utils.attr("recv_data")(res);
-    } 
+        
+        return tlm::TLM_ACCEPTED;
+    }
     
 };
 
@@ -204,6 +221,6 @@ void gen_tlm_data(int num)
 void recv_tlm_data(int num) 
 {
     
-    initiator.recv_tlm_data(num);
+    target.recv_tlm_data(num);
 
 }
