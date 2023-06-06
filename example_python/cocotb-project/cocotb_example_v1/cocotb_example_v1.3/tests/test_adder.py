6bb4a71ee97d07ad5d870d4e16762dd713227aa6
lx071 <onlyfor_ever@163.com>
2023-06-05 19:21:26 -0700

    update

--- example_python/cocotb-project/cocotb_example_v1/cocotb_example_v1.3/tests/test_adder.py
+++ example_python/cocotb-project/cocotb_example_v1/cocotb_example_v1.3/tests/test_adder.py
@@ -9,19 +9,19 @@ import random
 
 async def generate_clock(dut):
     """Generate clock pulses."""
-    dut.clk_i.value = 0
+    dut.clk.value = 0
     # for cycle in range(100):
     while True:
-        dut.clk_i.value = 0
+        dut.clk.value = 0
         await Timer(5, units="ns")
-        dut.clk_i.value = 1
+        dut.clk.value = 1
         await Timer(5, units="ns")
 
 
 async def generate_rst(dut):
     dut.reset_n.value = 0
     for cycle in range(10):
-        await RisingEdge(dut.clk_i)
+        await RisingEdge(dut.clk)
     dut.reset_n.value = 1
 
 @cocotb.test()
@@ -35,7 +35,7 @@ async def adder_basic_test(dut):
     await RisingEdge(dut.reset_n)
 
     for k in range(2000000):
-        await RisingEdge(dut.clk_i)
+        await RisingEdge(dut.clk)
         dut.start.value = 1
         dut.A.value = k % 200
         dut.B.value = k % 200
